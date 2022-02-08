41707ef12a61484cc20e1d1f9d6c16a4cb770fec
Gagan Batra <lucifer14.gb@gmail.com>
2022-02-08 15:50:49 -0500

    Changing for merging conflict

--- hellogit.c
+++ hellogit.c
@@ -3,4 +3,7 @@
 
 int main(){
     printf("Hello Git");
+
+    printf("Changed in branch but did not refect in master, since we did not merge yet. But changing this file purposefully for merging
+    conflicts");
 }
